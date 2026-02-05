9800efa37ab6c7f89d0ee414a802f6fe8dbd509c
9thIvy <180355498+9thIvy@users.noreply.github.com>
2026-02-04 19:12:42 -0800

    Prototype upload following rocket tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::data::{Data, ToByteUnit};
+use rocket::http::uri::Absolute;
+#[macro_use]
+extern crate rocket;
+mod paste_id;
+
+// In a real application, these would be retrieved dynamically from a config.
+// Also, host and port are defined in Rocket.toml
+// Either get those values and use them in HOST or maybe seperate config file?
+const ID_LENGTH: usize = 5;
+const HOST: Absolute<'static> = uri!("http://localhost:5432");
+
+// Eventually we will want to return actual json.
+#[post("/", data = "<paste>")]
+async fn upload(paste: Data<'_>) -> std::io::Result<String> {
+    let id = paste_id::PasteId::new(ID_LENGTH);
+    match paste.open(128.kibibytes()).into_file(id.file_path()).await {
+        Ok(_) => Ok(format!("Success")),
+        Err(e) => Ok(format!("Error: {}", e)),
+    }
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+
+      POST /
+
+          accepts raw data in the body of the request and responds with a URL of
+          a page containing the body's content
+    "
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, upload])
 }
---
+++ src/paste_id.rs
@@ -0,0 +1,30 @@
+use rand::prelude::*;
+use std::borrow::Cow;
+use std::path::{Path, PathBuf};
+
+#[derive(UriDisplayPath)]
+pub struct PasteId<'a>(Cow<'a, str>);
+
+impl PasteId<'_> {
+    /// Generate a _probably_ unique ID with `size` characters. For readability,
+    /// the characters used are from the sets [0-9], [A-Z], [a-z]. The
+    /// probability of a collision depends on the value of `size` and the number
+    /// of IDs generated thus far.
+    pub fn new(size: usize) -> PasteId<'static> {
+        const BASE62: &[u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        let mut id = String::with_capacity(size);
+        let mut rng = rand::rng();
+        for _ in 0..size {
+            id.push(BASE62[rng.random_range(0..63)] as char);
+        }
+
+        PasteId(Cow::Owned(id))
+    }
+
+    /// Returns the path to the paste in `upload/` corresponding to this ID.
+    pub fn file_path(&self) -> PathBuf {
+        let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
+        Path::new(root).join(self.0.as_ref())
+    }
+}
